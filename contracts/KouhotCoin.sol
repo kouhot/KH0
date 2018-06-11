883c8bc6a1f9fb26f0265be6b76085b49cd675ed
kouhot <kouhot6210@gmail.com>
2018-06-12 00:51:25 +0900

    Enable to deploy Ropsten

--- contracts/KouhotCoin.sol
+++ contracts/KouhotCoin.sol
@@ -1,8 +1,13 @@
 pragma solidity ^0.4.19;
-import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
-contract KouhotCoin is MintableToken {
+contract KouhotCoin is StandardToken {
   string public name = "KOUHOT COIN";
   string public symbol = "KH0";
   uint8 public decimals = 18;
+
+  function KouhotCoin(uint initialSupply) public {
+    totalSupply_ = initialSupply;
+    balances[msg.sender] = initialSupply;
+  }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,24 +1,7 @@
 const KouhotCoinCrowdsale = artifacts.require('./KouhotCoinCrowdsale.sol');
 const KouhotCoin = artifacts.require('./KouhotCoin.sol');
 
-module.exports = function(deployer, network, accounts) {
-    const openingTime = web3.eth.getBlock('latest').timestamp + 2; // two secs in the future
-    const closingTime = openingTime + 86400 * 20; // 20 days
-    const rate = new web3.BigNumber(1000);
-    const wallet = accounts[1];
-
-    return deployer
-        .then(() => {
-            return deployer.deploy(KouhotCoin);
-        })
-        .then(() => {
-            return deployer.deploy(
-                KouhotCoinCrowdsale,
-                openingTime,
-                closingTime,
-                rate,
-                wallet,
-                KouhotCoin.address
-            );
-        });
-};
+module.exports = (deployer) => {
+  const initialSupply = 50000e18
+  deployer.deploy(KouhotCoin, initialSupply)
+}
