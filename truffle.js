826363299e72b5751ce6e5d13d9d12b755e0765f
kouhot <kouhot6210@gmail.com>
2018-06-11 22:57:53 +0900

    Update

--- truffle.js
+++ truffle.js
@@ -1,3 +1,7 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = process.env.ROPSTEN_MNEMONIC;
+var accessToken = process.env.INFURA_ACCESS_TOKEN;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   networks: {
@@ -5,6 +9,16 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          "https://ropsten.infura.io/" + accessToken
+        );
+      },
+      network_id: 3,
+      gas: 40000
     }
   }
 };
